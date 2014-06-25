d9a471a12750ec473fbfabfcc2e66b6e2cf9bf96
jtarlecki <jtarlecki@yahoo.com>
2014-06-25 01:38:15 -0400

    generic function loop through time-intervarls, new jquery

--- beta/modular_clock.js
+++ beta/modular_clock.js
@@ -2,52 +2,38 @@ var clockID = 0;
 var times = new Array(3);
 var fiftynine=59;
 var twentyfour=24;
-var timeVars = ['secs', 'mins', 'hours'];
+
+//We loop through these id_names a bunch
+function timeVarsLoop(func){
+	$.each(['secs', 'mins', 'hours'], function(index, value) { 	
+		//console.log(func);
+		func(value);
+	});
+}
 
 function modular_clock_init(){
 	
-	//plan to loop a lot-- make generic iterator that accept functions
-	function timeVarsLoop(func){
-		$.each(timeVars, function(index, value) { 	
-			func(value);
-		});
-	}
-	
-	//Add divs for hours, mins, secs.	
-	$('#modular_clock').append('<div id="hours" class="third"></div><div id="mins" class="third"></div><div id="secs" class="third"></div>');
-	
-	/*
-	for (i=0; i < 54; i++){
-		$.each(timeVars, function(index, value) { 	
-			$('#'+value).append('<div class="box box_' + value + '"></div>')
-		});
-	}
-	*/
-	
-	var a = function addDivs(id) {
+	var childDivs = function childDivsFn(id) {
 		$('#'+id).append('<div class="box box_' + id + '"></div>')
 	}
-	for (i=0; i < 54; i++){
-		timeVarsLoop(a);
-	}
-	
-	
-	//Add select inputs.
-	var b = function selectBox(id) {
+	var selects = function selectFn(id) {
 		var options
-		for (i=10; i>1; i--) {options += '<option value="' + i + '">' + i + '</option>';}
+		for (i=10; i>1; i--) {
+			options += '<option value="' + i + '">' + i + '</option>';
+		}
 		dv = '<div class="box_mod"><select id="mod_' + id + '">' + options + '</div>';
 		$('#'+id).append(dv);
 	}
 	
-	timeVarsLoop(b);
-	/*
-	$.each(timeVars, function(index, value) { 	
-		selectBox(value);
-	});
-	*/
+	//Add divs for hours, mins, secs.	
+	$('#modular_clock').append('<div id="hours"></div><div id="mins"></div><div id="secs"></div>')
+	$("div div").addClass("third");
 	
-
+	//Create child divs
+	for (i=0; i < 54; i++){timeVarsLoop(childDivs);}
+		
+	//Add select inputs.
+	timeVarsLoop(selects);
 	
 	//Start the clock
 	modular_clock_update(1);
@@ -56,23 +42,16 @@ function modular_clock_init(){
 
 function modular_clock_update( refresh ){
 	
-	//alert('modular_clock_update');
-	
 	//Clear the clock.
 	if(clockID) {
 	  clearTimeout(clockID);
 	  clockID  = 0;
 	}
 	
-	//If refresh == 1, reset styles.
 	if (refresh == 1){
-		for (i=0; i < 54; i++){
-			$.each(timeVars, function(index, value) { 	
-				document.getElementById(value).childNodes[i].className="box box_"+value;
-			});
-		}
+		timeVarsLoop(function styleResetFunc(id){$('[class*="box box"]').attr('class', "box box_"+id)});
 	}
-
+		
 	//Make and pad array of times.
 	var tDate = new Date();
 	times[0] = tDate.getSeconds().toString( $('#mod_secs').val() );
@@ -86,23 +65,21 @@ function modular_clock_update( refresh ){
 	}
 
 	//Clock object 
-	function Clock(id, number, zeroTest, k, eq) {
+	function Clock(id, number, zeroTest, k, toggleClassNameCondition) {
 		this.id = id;
 		this.zeroTest = ( zeroTest || refresh == 1);
 		this.k = k;
-		this.eq = eq;
+		this.toggleClassNameCondition = toggleClassNameCondition;
 		this.mod = '#mod_' + id;
 		this.i_condition = $(this.mod).val() - 1;
 		this.j_condition = number.toString( $(this.mod).val() ).length - 1;
-		
-		console.log(this.eq)
 		
 		this.childIndex = function(i,j){
 			return this.k - j - ( 6 * i );
 		}
 		
 		this.toggleClassName = function(i,j){
-			return "box box_"+((this.eq(i,j)) ? "on" : "off");
+			return "box box_"+((this.toggleClassNameCondition(i,j)) ? "on" : "off");
 		}
 		
 		this.toggleClass = function(i,j){
@@ -115,7 +92,7 @@ function modular_clock_update( refresh ){
 			}
 		}
 		
-		this.updateColumns = function() {
+		this.updateColumns = function(){
 			if (this.zeroTest){
 				for (i=0; i < this.i_condition; i++){
 					for (j=0; j < this.j_condition; j++){
@@ -126,7 +103,6 @@ function modular_clock_update( refresh ){
 		}		
 	}
 	
-	 
 	var ones = new Clock( 'secs', '', '', 53, function (i,j) {return i < times[0][5]} );
 	var secs = new Clock( 'secs', fiftynine, (times[0][5] == 0), 52, function (i,j) {return i < times[0][ ( 4-j ) ]} );
 	var mins = new Clock( 'mins', fiftynine, (times[0] == "000000"), 53, function (i,j) {return i < times[1][ ( 5-j ) ]} );
